f36e324f41c550ba440152cc8a313e9d9ffb6a35
Vitaliy Ogarko <vogarko@gmail.com>
2018-08-21 11:13:39 +0800

    Workaround to stop dlg process

--- dlg2influx/run_dlg_with_node_exporter.sh
+++ dlg2influx/run_dlg_with_node_exporter.sh
@@ -5,17 +5,14 @@ node_exporter="$HOME/node_exporter-0.16.0.linux-amd64/node_exporter"
 
 # Run node exporter.
 $node_exporter &
-
 node_exporter_pid=$!
 
 # Run DALiuGE (on the same node with node exporter).
 "$@" &
-
 dlg_pid=$!
 
-wait $dlg_pid
+# TODO: Workaround, before proper stopping of DALiuGE gets implemened.
+sleep 60
+kill -9 $dlg_pid
 
 kill $node_exporter_pid
-wait $node_exporter_pid
-
-echo "OLOLO"
