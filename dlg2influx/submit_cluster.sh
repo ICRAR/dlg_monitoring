699eaf1e41e45524b5e5cf7907b1babee098484c
Vitaliy Ogarko <vogarko@gmail.com>
2018-08-21 15:11:05 +0800

    Write system data to InfluxDB via Prometheus

--- dlg2influx/submit_cluster.sh
+++ dlg2influx/submit_cluster.sh
@@ -8,11 +8,24 @@
 # Main parameters.
 #-------------------------------------------------
 # Python interpreter path.
-PYTHON="$HOME/test_venv/bin/python"
+python="$HOME/test_venv/bin/python"
 
 # Path to a logical graph.
-#LOGICAL_GRAPH_PATH="$HOME/dlg_monitoring/graphs/Vitaliy_long.graph"
-LOGICAL_GRAPH_PATH="$HOME/dlg_monitoring/graphs/Vitaliy_scatter.graph"
+logical_graph="$HOME/dlg_monitoring/graphs/Vitaliy_scatter.graph"
+
+# DALiuGE event listener.
+dlg_listener="$HOME/dlg_monitoring/dlg2influx/dlg2influx.py" 
+dlg_listener_class="dlg2influx.Listener"
+
+# Graph parametrizer.
+graph_parametrizer=$dlg_listener
+
+# Path to the srun script.
+srun_script="$HOME/dlg_monitoring/dlg2influx/run_dlg_with_node_exporter.sh"
+
+#-------------------------------------------------
+# InfluxDB parameters.
+#-------------------------------------------------
 
 # InfluxDB access parameters.
 export INFLUXDB_HOST="ec2-54-159-33-236.compute-1.amazonaws.com"
@@ -21,22 +34,32 @@ export INFLUXDB_NAME="daliuge"
 export INFLUXDB_USER=""
 export INFLUXDB_PASSWORD=""
 
-# DALiuGE event listener.
-EVENT_LISTENER_PATH="$HOME/dlg_monitoring/dlg2influx/dlg2influx.py" 
-EVENT_LISTENER_CLASS="dlg2influx.Listener"
+#-------------------------------------------------
+# Prometheus parameters.
+#-------------------------------------------------
+# Target listen port. (Use the port number exposed by the node exporter)
+prometheus_target_port=9100
 
-# Graph parametrizer.
-PARAMETRIZER_PATH=$EVENT_LISTENER_PATH
+# Path to the template config file.
+prometheus_template_config="$HOME/dlg_monitoring/dlg2influx/prometheus_dlg_template.yml"
 
-# Path to the srun script.
-SRUN_SCRIPT_PATH="$HOME/dlg_monitoring/dlg2influx/run_dlg_with_node_exporter.sh"
+# Data scraping interval (sec).
+prometheus_scraping_interval=1
+
+# Path to Prometheus application.
+prometheus_app="./prometheus-2.3.1.linux-amd64/prometheus"
+# Prometheus server listen address.
+prometheus_listen_address="0.0.0.0:8080"
+# Path to the Prometheus database.
+prometheus_db_path="/tmp/dlg-prometheus-data/"
 
 #-------------------------------------------------
 # Extract sha-value from the graph.
 #-------------------------------------------------
-SHA=$(sed -r -n 's/.*"sha": *"([a-z0-9]+)".*/\1/p' $LOGICAL_GRAPH_PATH)
-echo "SHA=$SHA"
-export GRAPH_SHA=$SHA
+sha=$(sed -r -n 's/.*"sha": *"([a-z0-9]+)".*/\1/p' $logical_graph)
+echo "sha=$sha"
+
+export GRAPH_SHA=$sha
 
 #-------------------------------------------------
 # Loading modules.
@@ -48,33 +71,87 @@ module load python/2.7.14
 # to load system python 2.7 module first to make sure everything is in 2.7
 module load mpi4py
 
+#--------------------------------------------------
+# Generating the list of Prometheus targets.
+#--------------------------------------------------
+#SLURM_JOB_NODELIST='nid000[11-13]'
+echo $SLURM_JOB_NODELIST
+hosts=$(scontrol show hostnames $SLURM_JOB_NODELIST)
+targets=($hosts)
+
+hostspec=()
+for host in ${targets[*]}
+do
+    hostspec+=("'$host:$prometheus_target_port'")
+done
+
+prometheus_targets=$(echo "${hostspec[*]}" | sed 's/ /, /g')
+
+echo $prometheus_targets
+
+#---------------------------------------------------
+# Generating Prometheus config file.
+#---------------------------------------------------
+# Prometheus database credentials.
+prometheus_db_name=$INFLUXDB_NAME
+prometheus_user=$INFLUXDB_USER
+prometheus_password=$INFLUXDB_PASSWORD
+
+remote_write_url="${INFLUXDB_HOST}:${INFLUXDB_PORT}/api/v1/prom/write?u=${prometheus_user}&p=${prometheus_password}&db=${prometheus_db_name}"
+
+# Auto-generated config file path.
+prometheus_config="$HOME/prometheus_dlg.yml"
+
+# Generate the Prometheus config file from a template.
+sed "s/__TARGETS__/$prometheus_targets/;s/__REMOTE_WRITE_URL__/$remote_write_url/;s/__INTERVAL__/${prometheus_scraping_interval}s/g" $prometheus_template_config > $prometheus_config
+
+#---------------------------------------------------
+# Running Prometheus server.
+#---------------------------------------------------
+mkdir -p $prometheus_db_path
+
+run_command="$prometheus_app --config.file=$prometheus_config --web.listen-address=$prometheus_listen_address --storage.tsdb.path=$prometheus_db_path"
+
+# Enabling debug logging.
+run_command="$COMMAND --log.level=debug"
+
+echo $run_command
+$run_command &
+
+prometheus_pid=$!
+
 #-----------------------------------------------------
 # Translate LG to PLG (Parametrized Logical Graph).
 #-----------------------------------------------------
-TEMP_FOLDER="./tmp"
-SID=$(date +"%Y-%m-%d_%H-%M-%S")
-mkdir -p $TEMP_FOLDER
-PARAMETRIZED_GRAPH_PATH="$TEMP_FOLDER/PLG_${SID}.graph"
-$PYTHON $PARAMETRIZER_PATH -L $LOGICAL_GRAPH_PATH -o $PARAMETRIZED_GRAPH_PATH
+temp_dir="./tmp"
+sid=$(date +"%Y-%m-%d_%H-%M-%S")
+mkdir -p $temp_dir
+parametrized_graph="$temp_dir/PLG_${sid}.graph"
+
+$python $graph_parametrizer -L $logical_graph -o $parametrized_graph
 
 #--------------------------------------------------
 # Runing DALiuGE.
 #--------------------------------------------------
-SID=$(date +"%Y-%m-%d_%H-%M-%S")
+sid=$(date +"%Y-%m-%d_%H-%M-%S")
 # Folder for storing logs.
-LOG_DIR="$HOME/dlg-logs/"$SID
+dlg_log_dir="$HOME/dlg-logs/"$sid
 
-mkdir -p $LOG_DIR # To remove potential directory creation conflicts later.
+mkdir -p $dlg_log_dir # To remove potential directory creation conflicts later.
 
 # Defualt DALiuGE lib path.
-DLG_LIB_PATH="$HOME/.dlg/lib"
+dlg_lib_path="$HOME/.dlg/lib"
 
 # Copy event listener to DALiuGE libs folder.
-cp $EVENT_LISTENER_PATH $DLG_LIB_PATH
+cp $dlg_listener $dlg_lib_path
 
-srun --export=all $SRUN_SCRIPT_PATH $PYTHON -m dlg.deploy.pawsey.start_dfms_cluster -l $LOG_DIR -L $PARAMETRIZED_GRAPH_PATH --event-listener=$EVENT_LISTENER_CLASS
+srun --export=all $srun_script $python -m dlg.deploy.pawsey.start_dfms_cluster -l $dlg_log_dir -L $parametrized_graph --event-listener=$dlg_listener_class
 
 #-------------------------------------------------
+# Kill Prometheus server (forcing it to write its data).
+kill $prometheus_pid
+wait $prometheus_pid
+
 echo "FINISHED!"
 
 
